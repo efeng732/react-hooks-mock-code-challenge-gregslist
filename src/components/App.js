82bddbb2de045d57a8ecd19e40548dc3869d8407
Erwin <efeng732@gmail.com>
2021-02-09 08:49:46 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
+import {useState, useEffect} from "react";
 
 function App() {
+  const[listings, setListings] = useState([])
+  const [search, setSearch] = useState("")
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then(res => res.json())
+    .then(listingArray => setListings(listingArray))
+  }, [])
+
+  function deleteListing (deletedId) {
+    const updatedListings = listings.filter((listing) => listing.id !== deletedId)
+    setListings(updatedListings)
+  }
+
+  const displayedListings = listings.filter((listing) => listing.description.toLowerCase().includes(search.toLowerCase()))
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header setSearch={setSearch} />
+      <ListingsContainer deleteListing={deleteListing} listings={displayedListings} />
     </div>
   );
 }
